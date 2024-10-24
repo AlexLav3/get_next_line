562d22f939347aab2643edb941510375dd106fe2
Elena Lavricheva <elavrich@c2r1s5.42prague.com>
2024-10-24 21:52:05 +0200

    let's see

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 21:39:08 by elavrich          #+#    #+#             */
+/*   Updated: 2024/10/20 15:03:47 by elavrich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strlen(char *str);
+char	*cut_line(char *str);
+char	*get_rest(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*read_from_f(int fd, char *read_line);
+int		ft_strchr(const char *s, char c);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 21:39:29 by elavrich          #+#    #+#             */
+/*   Updated: 2024/10/20 15:02:11 by elavrich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*concat;
+
+	concat = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!concat)
+		return (free(s1), NULL);
+	i = -1;
+	while (s1[++i])
+		concat[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		concat[i + j] = s2[j];
+	concat[i + j] = '\0';
+	free(s1);
+	return (concat);
+}
+
+int	ft_strchr(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
